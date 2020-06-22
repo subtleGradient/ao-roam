c63a5724bdc68ce31e057d9d35feda6dd4b6368f
Thomas Aylott <oblivious@subtlegradient.com>
2020-06-22 16:54:37 -0400

    vivify

--- index.js
+++ index.js
@@ -27,23 +27,17 @@ export const getIdForTitle = (/**@type {string} */ title) =>
 export const getParentId = (/**@type {number} */ dbid) =>
   q("[:find ?e :in $ ?a :where [?e :block/children ?a]]", dbid)[0][0]
 
+export const getIdFromUid = (/**@type {string} */ uid) =>
+  q("[:find ?e :in $ ?a :where [?e :block/uid ?a]]", uid)[0][0]
+
 export const getCurrentPageUid = () =>
   window.location.hash.split("/").reverse()[0]
 
 export const getCurrentPage = () =>
-  pull(
-    "[*]",
-    q(
-      "[:find ?e :in $ ?a :where [?e :block/uid ?a]]",
-      getCurrentPageUid(),
-    )[0][0],
-  )
+  pull("[*]", getIdFromUid(getCurrentPageUid()))
 
 export const getStuffThatRefsTo = (/**@type {string} */ title) =>
   getStuffThatRefsToId(getIdForTitle(title))
-
-export const uidFromElement = (/**@type {Element} */ element) =>
-  element.id.split("-").reverse()[0] //id="block-input-F6uIztpC2xbzqDVDuu32IJReoeW2-body-outline-alyAURK40-0unKRxaGp"
 
 export const getUrlToUid = (/**@type {string} */ uid) =>
   window.location.toString().replace(getCurrentPageUid(), uid)
@@ -84,11 +78,22 @@ export const executeBlockById = (/**@type {number} */ dbid) => {
   })
 }
 
+export const uidFromElement = (/**@type {Element} */ element) =>
+  element.id.split("-").reverse()[0] //id="block-input-F6uIztpC2xbzqDVDuu32IJReoeW2-body-outline-alyAURK40-0unKRxaGp"
+
 const observer = new MutationObserver((mutationsList, observer) => {
   for (const mutation of mutationsList) {
     if (mutation.type === "childList") {
-      // console.log("A child node has been added or removed.")
-      // mutation.addedNodes[0]
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          const uid = uidFromElement(
+            /**@type {Element}*/ (/**@type {any}*/ node),
+          )
+          if (!uid) return
+          console.log("mounted block", { ":block/uid": uid })
+        }
+      })
+
       // mutation.removedNodes
       // } else if (mutation.type === "attributes") {
       // console.log("The " + mutation.attributeName + " attribute was modified.")
@@ -101,7 +106,7 @@ export const roam_onInit = () => {
     setTimeout(roam_onInit, 100)
     return
   }
-  executeEverythingThatRefsTo("ao/js/roam/onInit")
+  executeEverythingThatRefsTo("vivify/onInit")
 
   // observer.observe(document.documentElement, {
   //   attributes: true,
