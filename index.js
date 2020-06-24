9680872e58357a6e7c55a8f3621173c8e1d5e1f3
Thomas Aylott <oblivious@subtlegradient.com>
2020-06-24 19:29:43 -0400

    ++

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import { React, ReactDOM } from "https://unpkg.com/es-react@16"
+import htm from "https://unpkg.com/htm?module"
+
+const html = htm.bind(React.createElement)
+
+const Counter = props => {
+  const [count, setCount] = React.useState(parseInt(props.count))
+  return html`
+    <div>
+      <h1>${count}</h1>
+      <button onClick=${e => setCount(count - 1)}>Decrement</button>
+      <button onClick=${e => setCount(count + 1)}>Increment</button>
+    </div>
+  `
+}
+
 console.debug("executing ao-roam index DEV")
 
 const nao = Date.now()
@@ -206,6 +222,22 @@ export const roam_onInit = () => {
 
   // Later, you can stop observing
   // observer.disconnect();
+  initRenderStuff()
+}
+
+function initRenderStuff() {
+  const root =
+    document.getElementsByTagName("vivify-root")[0] ||
+    document.createElement("vivify-root")
+  document.body.appendChild(root)
+
+  ReactDOM.render(
+    html`
+      <h1>Look Ma! No script tags, no build step</h1>
+      <${Counter} count=${0} />
+    `,
+    root,
+  )
 }
 
 const vivify = {
