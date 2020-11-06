21a177c28ddb6283abe69e000dc667fd3810cd07
Thomas Aylott <oblivious@subtlegradient.com>
2020-11-06 16:16:26 -0500

    +importUMD

---
+++ importUMD.js
@@ -0,0 +1,10 @@
+/**
+ * @param {RequestInfo} url
+ */
+export default async (url, module = { exports: {} }) =>
+  (Function("module", "exports", await (await fetch(url)).text()).call(
+    module,
+    module,
+    module.exports,
+  ),
+  module).exports
